f449dc4f9e08c2d4f8fb745f102615ac5fba9b1f
Bocai_BCA <105545253+bocai-bca@users.noreply.github.com>
2024-03-06 07:52:03 +0800

    Node script path re-bind

--- bca_godot_lib/GDScript/2D/Area2D/generic_rectangle_hitbox/generic_rectangle_hitbox.gd
+++ bca_godot_lib/GDScript/2D/Area2D/generic_rectangle_hitbox/generic_rectangle_hitbox.gd
@@ -8,14 +8,11 @@ func _ready()->void:
 	set_shape(_shape_0.shape.size)
 
 func set_offset(last_tick_pos_offset:Vector2, last_tick_rot_offset:float)->void:
-	#step 1 is finish because node "true_shape" is rotation == 0.0 always
 	_shape_1.set_position(last_tick_pos_offset)
 	_shape_1.set_rotation(last_tick_rot_offset)
 	if (last_tick_pos_offset == Vector2(0.0, 0.0)):
 		_offset.polygon = PackedVector2Array([Vector2(), Vector2(), Vector2(), Vector2()])
-	#function
 	var size:Vector2 = _shape_0.shape.size
-	#step2, get diagonals of recA and recB
 	var a_diagonals:Array[diagonal] = [
 		diagonal.new(Vector2(size.x / -2.0, size.y / -2.0), Vector2(size.x / 2.0, size.y / 2.0)),
 		diagonal.new(Vector2(size.x / -2.0, size.y / 2.0), Vector2(size.x / 2.0, size.y / -2.0))
@@ -33,41 +30,33 @@ func set_offset(last_tick_pos_offset:Vector2, last_tick_rot_offset:float)->void:
 	var b_diagonal_angle_0:float = abs(tan(b_diagonals[0].point_0.angle_to_point(b_diagonals[0].point_1)))
 	var b_diagonal_angle_1:float = abs(tan(b_diagonals[1].point_0.angle_to_point(b_diagonals[1].point_1)))
 	var b_use_diagonal:int = 0
-	#step3, connect A with B, and check which B's diagonal is more vertical to line-AB.
 	var line_ab:Vector2 = Vector2(0.0, 0.0).direction_to(last_tick_pos_offset)
 	var line_ab_ortho:Vector2 = line_ab.orthogonal()
 	if (last_tick_rot_offset != 0.0):
-		#step 4, get the most vertical diagonal's two points.
 		var b_diagonal_0_vertical:float = abs(fmod(line_ab_ortho.angle() - b_diagonal_angle_0, PI))
 		var b_diagonal_1_vertical:float = abs(fmod(line_ab_ortho.angle() - b_diagonal_angle_1, PI))
 		if (b_diagonal_0_vertical > b_diagonal_1_vertical):
 			b_use_diagonal = 1
 		else:
 			b_use_diagonal = 2
-	#step 5, get A's diagonal
 	var a_use_diagonal:int
 	var ab_angle:float = line_ab.angle() + PI
-	if (0.0 <= ab_angle and ab_angle < PI/2.0): #第四象限
+	if (0.0 <= ab_angle and ab_angle < PI/2.0):
 		a_use_diagonal = 2
-		#step 5.5, get B's diagonal if recB is axis aligned
 		if (b_use_diagonal == 0):
 			b_use_diagonal = 2
-	elif (PI/2.0 <= ab_angle and ab_angle < PI): #第三象限
+	elif (PI/2.0 <= ab_angle and ab_angle < PI):
 		a_use_diagonal = 1
-		#step 5.5, get B's diagonal if recB is axis aligned
 		if (b_use_diagonal == 0):
 			b_use_diagonal = 1
-	elif (PI <= ab_angle and ab_angle < PI*1.5): #第二象限
+	elif (PI <= ab_angle and ab_angle < PI*1.5):
 		a_use_diagonal = 2
-		#step 5.5, get B's diagonal if recB is axis aligned
 		if (b_use_diagonal == 0):
 			b_use_diagonal = 2
-	else: #第一象限
+	else:
 		a_use_diagonal = 1
-		#step 5.5, get B's diagonal if recB is axis aligned
 		if (b_use_diagonal == 0):
 			b_use_diagonal = 1
-	#step6, get all using points and sort them by oclock.
 	var all_using_points:Array = []
 	all_using_points.append(Dictionary({angle = 0.0, point = a_diagonals[a_use_diagonal - 1].point_0}))
 	all_using_points.append(Dictionary({angle = 0.0, point = a_diagonals[a_use_diagonal - 1].point_1}))
@@ -79,12 +68,10 @@ func set_offset(last_tick_pos_offset:Vector2, last_tick_rot_offset:float)->void:
 		func(a, b):
 			return a.angle < b.angle
 	)
-	#step7, output
 	for x in all_using_points.size():
 		_offset.polygon.resize(all_using_points.size())
 		_offset.polygon.fill(Vector2(0.0, 0.0))
 		_offset.polygon[x-1] = Vector2(all_using_points[x-1].point)
-	#/function
 
 func set_shape(size:Vector2)->void:
 	_shape_0.shape.size = size
